3fd264a060cf702bceb9c2e858c75bdefb047dad
brayan <brayan.challco.ticona2@gmail.com>
2026-06-17 09:54:06 -0400

    agregando codigo ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
+import Script from 'next/script'; // 👈 Importar Script de Next.js
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,6 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3770775034295435"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${inter.className} min-h-screen flex flex-col pt-16`}>
         <Header />
         <main className="flex-1">
